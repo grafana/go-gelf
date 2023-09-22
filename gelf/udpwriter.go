d426cd7af3f50bdd67d1912ba124cb7a6df6a58e
Ivan Kukhta <ivan.kukhta@acronis.com>
2023-09-22 17:25:14 +0300

    Move magic/chunk into separate file

--- gelf/udpwriter.go
+++ gelf/udpwriter.go
@@ -34,35 +34,6 @@ const (
 	CompressNone
 )
 
-// Used to control GELF chunking.  Should be less than (MTU - len(UDP
-// header)).
-//
-// TODO: generate dynamically using Path MTU Discovery?
-const (
-	ChunkSize        = 1420
-	chunkedHeaderLen = 12
-	chunkedDataLen   = ChunkSize - chunkedHeaderLen
-	// maxChunksCount is limited by the protocol to a maximum of 128
-	// https://docs.graylog.org/docs/gelf#gelf-via-udp
-	maxChunksCount = 128
-)
-
-var (
-	magicChunked = []byte{0x1e, 0x0f}
-	magicZlib    = []byte{0x78}
-	magicGzip    = []byte{0x1f, 0x8b}
-)
-
-// numChunks returns the number of GELF chunks necessary to transmit
-// the given compressed buffer.
-func numChunks(b []byte) int {
-	lenB := len(b)
-	if lenB <= ChunkSize {
-		return 1
-	}
-	return len(b)/chunkedDataLen + 1
-}
-
 // New returns a new GELF Writer.  This writer can be used to send the
 // output of the standard Go log functions to a central GELF server by
 // passing it to log.SetOutput()
@@ -87,8 +58,8 @@ func NewUDPWriter(addr string) (*UDPWriter, error) {
 // of GELF chunked messages.  The format is documented at
 // http://docs.graylog.org/en/2.1/pages/gelf.html as:
 //
-//     2-byte magic (0x1e 0x0f), 8 byte id, 1 byte sequence id, 1 byte
-//     total, chunk-data
+//	2-byte magic (0x1e 0x0f), 8 byte id, 1 byte sequence id, 1 byte
+//	total, chunk-data
 func (w *GelfWriter) writeChunked(zBytes []byte) (err error) {
 	b := make([]byte, 0, ChunkSize)
 	buf := bytes.NewBuffer(b)
